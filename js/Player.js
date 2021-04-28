008f079f94444dd14c69f677b36ac15001f5ede1
ArihantMaity <73533612+ArihantMaity@users.noreply.github.com>
2021-04-28 16:11:15 +0530

    Add files via upload

---
+++ js/Player.js
@@ -0,0 +1,51 @@
+class Player {
+  constructor(){
+    this.index = null;
+    this.distance = 0;
+    this.name = null;
+    this.rank = 0;
+  }
+
+  getCount(){
+    var playerCountRef = database.ref('playerCount');
+    playerCountRef.on("value",(data)=>{
+      playerCount = data.val();
+    })
+  }
+
+  updateCount(count){
+    database.ref('/').update({
+      playerCount: count
+    });
+  }
+
+  update(){
+    var playerIndex = "players/player" + this.index;
+    database.ref(playerIndex).set({
+      name:this.name,
+      distance:this.distance,
+      rank:this.rank
+    });
+  }
+
+  getCars(){
+    var getCarsAtEndRef = database.ref("carsAtEnd");
+    getCarsAtEndRef.on("value",(data)=>{
+      rankRef = data.val();
+    })
+  }
+
+  static updateCars(){
+   database.ref("/").update({
+     carsAtEnd : rankRef+1
+   })
+  this.rank+=1;
+  }
+
+  static getPlayerInfo(){
+    var playerInfoRef = database.ref('players');
+    playerInfoRef.on("value",(data)=>{
+      allPlayers = data.val();
+    })
+  }
+}
